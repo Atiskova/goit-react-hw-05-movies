d92cf7f0f65b8b384923371f8cc6fbb1672892f7
Anna Atiskova <anna.atiskova@gmail.com>
2023-02-27 03:38:18 +0200

    fix style

--- src/views/MovieDetail.styled.js
+++ src/views/MovieDetail.styled.js
@@ -2,10 +2,12 @@ import styled from 'styled-components';
 
 export const Container = styled.section`
   border-bottom: 1px solid grey;
+  padding: 30px 40px;
 `;
 
 export const Title = styled.h2`
   margin-left: 20px;
+  margin-top: 0;
 `;
 export const ListItem = styled.li`
   margin-bottom: 10px;
